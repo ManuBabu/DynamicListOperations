440f2a96e72a40302c657ff90b8eddcf02ec46eb
ManuBabu <manubabu2012@yahoo.in>
2015-12-16 07:23:56 +0530

    Dynamic list operations
    inserting and deleting of elements dynalicaly

---
+++ app/src/main/java/com/apps/manu/dynamiclistoperations/MainActivity.java
@@ -0,0 +1,171 @@
+package com.apps.manu.dynamiclistoperations;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.drawable.Drawable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    public class Item {
+
+        String ItemString;
+        Item( String t){
+
+            ItemString = t;
+        }
+    }
+
+    static class ViewHolder {
+        ImageView icon;
+        TextView text;
+    }
+
+    public class ItemsListAdapter extends BaseAdapter {
+
+        private Context context;
+        private List<Item> list;
+
+        ItemsListAdapter(Context c, List<Item> l){
+            context = c;
+            list = l;
+        }
+
+        @Override
+        public int getCount() {
+            return list.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return list.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View rowView = convertView;
+
+            // reuse views
+            if (rowView == null) {
+                LayoutInflater inflater = ((Activity) context).getLayoutInflater();
+                rowView = inflater.inflate(R.layout.row, null);
+
+                ViewHolder viewHolder = new ViewHolder();
+             //   viewHolder.icon = (ImageView) rowView.findViewById(R.id.rowImageView);
+                viewHolder.text = (TextView) rowView.findViewById(R.id.rowTextView);
+                rowView.setTag(viewHolder);
+            }
+
+            ViewHolder holder = (ViewHolder) rowView.getTag();
+            holder.text.setText(list.get(position).ItemString);
+
+            return rowView;
+        }
+
+        public List<Item> getList(){
+            return list;
+        }
+    }
+
+    List<Item> items1, items2;
+    ListView listView1, listView2;
+    ItemsListAdapter myItemsListAdapter1, myItemsListAdapter2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        listView1 = (ListView)findViewById(R.id.listview1);
+        listView2 = (ListView)findViewById(R.id.listview2);
+
+        initItems();
+        myItemsListAdapter1 = new ItemsListAdapter(this, items1);
+        myItemsListAdapter2 = new ItemsListAdapter(this, items2);
+        listView1.setAdapter(myItemsListAdapter1);
+        listView2.setAdapter(myItemsListAdapter2);
+
+        listView1.setOnItemClickListener(listOnItemClickListener);
+        listView2.setOnItemClickListener(listOnItemClickListener);
+
+        listView1.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener(){
+
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view,
+                                           int position, long id) {
+                Item selectedItem = (Item)(parent.getItemAtPosition(position));
+
+                ItemsListAdapter associatedAdapter = (ItemsListAdapter)(parent.getAdapter());
+                List<Item> associatedList = associatedAdapter.getList();
+                Item associatedItem = associatedList.get(position);
+                if(removeItemToList(associatedList, associatedItem)){
+
+                    view.invalidate();
+                    associatedAdapter.notifyDataSetChanged();
+                    ItemsListAdapter list2Adapter = (ItemsListAdapter)(listView2.getAdapter());
+                    List<Item> list2List = list2Adapter.getList();
+                    addItemToList(list2List, selectedItem);
+                    list2Adapter.notifyDataSetChanged();
+                }
+
+                return true;
+            }});
+
+    }
+
+    AdapterView.OnItemClickListener listOnItemClickListener = new AdapterView.OnItemClickListener(){
+
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position,
+                                long id) {
+            Toast.makeText(MainActivity.this,
+                    ((Item)(parent.getItemAtPosition(position))).ItemString,
+                    Toast.LENGTH_SHORT).show();
+        }
+
+    };
+
+    private void initItems(){
+        items1 = new ArrayList<Item>();
+        items2 = new ArrayList<Item>();
+
+        TypedArray arrayText = getResources().obtainTypedArray(R.array.restext);
+
+        for(int i=0; i<arrayText.length(); i++){
+            String s = arrayText.getString(i);
+            Item item = new Item( s);
+            items1.add(item);
+        }
+
+        arrayText.recycle();
+    }
+
+    private boolean removeItemToList(List<Item> l, Item it){
+        boolean result = l.remove(it);
+        return result;
+    }
+
+    private boolean addItemToList(List<Item> l, Item it){
+        boolean result = l.add(it);
+        return result;
+    }
+
+}
